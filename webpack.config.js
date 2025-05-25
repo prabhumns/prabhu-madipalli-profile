1160b33a2cd4048af51ebf905de437e4f026e2a3
Prabhu Madipalli <prabhumns123@gmail.com>
2025-05-25 06:45:37 +0530

    copy CNAME file to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -33,6 +34,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          globOptions: {
+            ignore: ["**/index.html"], // already handled by HtmlWebpackPlugin
+          },
+        },
+      ],
+    })
   ],
   devServer: {
     static: "./dist",
